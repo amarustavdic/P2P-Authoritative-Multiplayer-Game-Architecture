7ff90ee678ad351843b3b1ae06e995ee9523b6a6
Amar <amar.ustavdija@gmail.com>
2023-02-21 15:23:51 +0100

    .

--- src/main/scripts/test.sh
+++ src/main/scripts/test.sh
@@ -10,6 +10,6 @@ docker image rm $JAR_NAME:$JAR_VERSION
 docker build -t $JAR_NAME:$JAR_VERSION ../../../.
 
 # running it on different terminals
-gnome-terminal --geometry=60x15+50+50 --title="Bootstrap Node" -- docker run -it --rm -e ARG1="true" $JAR_NAME:$JAR_VERSION
+gnome-terminal --geometry=60x15+50+50 --title="Bootstrap Node" -- docker run --name bootstrp -it --rm -e ARG1="true" $JAR_NAME:$JAR_VERSION
 gnome-terminal --geometry=60x15+50+395 --title="Node 2" -- docker run -it --rm -e ARG1="false" $JAR_NAME:$JAR_VERSION
 gnome-terminal --geometry=60x15+50+740 --title="Node 3" -- docker run -it --rm -e ARG1="false" $JAR_NAME:$JAR_VERSION
