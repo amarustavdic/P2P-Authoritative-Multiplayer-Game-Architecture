701382c9f7a665f1bbc85168252a44280ebc5953
Amar <amar.ustavdija@gmail.com>
2023-05-15 07:05:54 +0200

    battlescene file

---
+++ src/main/java/com/mygame/app/ui/BattleScene.java
@@ -0,0 +1,26 @@
+package com.mygame.app.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BattleScene extends JPanel {
+
+
+
+    public BattleScene() {
+        setSize(GUIConstants.WINDOW_WIDTH, GUIConstants.WINDOW_HEIGHT);
+        setPreferredSize(new Dimension(GUIConstants.WINDOW_WIDTH, GUIConstants.WINDOW_HEIGHT));
+
+        setBackground(Color.RED);
+
+    }
+
+
+
+
+
+
+
+
+
+}
