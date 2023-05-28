ad0ff909eb488dfe2c25eb80baa583b8bf2f80f0
Amar <amar.ustavdija@gmail.com>
2023-05-28 04:21:17 +0200

    docker running, also opening the x server

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
-FROM openjdk:11
+FROM ubuntu:latest
+
+RUN apt-get update && apt-get install -y openjdk-19-jdk
+
 COPY target/app-1.0-SNAPSHOT-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
+
 CMD java -jar app-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/scripts/test.sh
+++ src/main/scripts/test.sh
@@ -3,19 +3,15 @@
 JAR_NAME=my-app
 JAR_VERSION=1.0
 
-# remove old containers and images
-# shellcheck disable=SC2046
+# Remove old containers and images
 docker rm -f $(docker ps -aq)
 docker image rm $JAR_NAME:$JAR_VERSION
+
+# Build the Docker image
 docker build -t $JAR_NAME:$JAR_VERSION ../../../.
 
-# running it on different terminals
-gnome-terminal --geometry=60x15+50+50 --title="Bootstrap Node" -- docker run --name bootstrp -it --rm -e ARG1="true" --ip="172.17.0.2" $JAR_NAME:$JAR_VERSION
-gnome-terminal --geometry=60x15+50+395 --title="Node 2" -- docker run -it --rm -e ARG1="false" $JAR_NAME:$JAR_VERSION
-gnome-terminal --geometry=60x15+50+740 --title="Node 3" -- docker run -it --rm -e ARG1="false" $JAR_NAME:$JAR_VERSION
-
-sleep 5
-
-
-# creating some additional containers, just to have a bit bigger network for  testing
+# Running containers in different terminals
+gnome-terminal --geometry=60x15+50+50 --title="Bootstrap Node" -- docker run --name bootstrp -it --rm -e ARG1="true" --ip="172.17.0.2" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro $JAR_NAME:$JAR_VERSION
+gnome-terminal --geometry=60x15+50+395 --title="Node 2" -- docker run -it --rm -e ARG1="false" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro $JAR_NAME:$JAR_VERSION
+gnome-terminal --geometry=60x15+50+740 --title="Node 3" -- docker run -it --rm -e ARG1="false" -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro $JAR_NAME:$JAR_VERSION
 
